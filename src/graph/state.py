0e03663449e43c9047c6340301d88091c76b14fe
Vamsi <mkrishnadev1996@gmail.com>
2026-05-02 15:53:17 +0530

    added new state attrib

--- src/graph/state.py
+++ src/graph/state.py
@@ -9,9 +9,11 @@ class GraphState(TypedDict):
         question: The users question
         generation: The generated answer from LLM
         is_web_search: If web search is required
+        search_queries: The list of queries for doing web search
         documents: The list of documents from the Vector db
     '''
     question: Annotated[str, Field(description="The users question")]
     generation: Annotated[str, Field(description="The generated answer from LLM")]
     is_web_search: Annotated[bool, Field(description="If web search is required")]
+    search_queries: Annotated[List[str], Field(description="The list of queries for doing web search as per user question")]
     documents: Annotated[List[Document], Field(description="The list of documents from the Vector db")]
