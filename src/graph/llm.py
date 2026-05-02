34fb5da0e6e0520978a568eb0226221a8708fdb3
Vamsi <mkrishnadev1996@gmail.com>
2026-05-02 20:32:59 +0530

    added new notes and respective chains and pudated state schema

--- src/graph/llm.py
+++ src/graph/llm.py
@@ -8,3 +8,10 @@ llm = ChatGroq(
     model=os.environ.get("GROQ_MODEL", "openai/gpt-oss-120b"),  # Default to a specific model if not set in .env
     api_key=os.environ.get("GROQ_API_KEY","NONE")
     )
+# Faster response times with lower quality output, suitable for generating search queries
+
+llm_fast=ChatGroq(
+    model=os.environ.get("GROQ_MODEL_FAST", "groq/compound-mini"),  # Default to a specific model if not set in .env
+    api_key=os.environ.get("GROQ_API_KEY","NONE"),
+    temperature=0.2
+)
