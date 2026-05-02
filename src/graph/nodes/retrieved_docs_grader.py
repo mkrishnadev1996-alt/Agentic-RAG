50dcd382b6694d871838432141995f81bb0b5a62
Vamsi <mkrishnadev1996@gmail.com>
2026-05-02 15:08:32 +0530

    added return type, comments

--- src/graph/nodes/retrieved_docs_grader.py
+++ src/graph/nodes/retrieved_docs_grader.py
@@ -1,20 +1,25 @@
+from typing import Dict, Any
 from src.graph.state import GraphState
 from src.graph.chains.grader import grader_chain
 
-def retrieved_docs_grader(state: GraphState):
+def retrieved_docs_grader(state: GraphState) -> Dict[str, Any]:
     docs = state.get('documents', [])
     if not docs:
         raise ValueError("Documents are not available for grading")
     question = state.get('question', '')
     filtered_docs = []
     is_web_search = False
+
+    # Grade each document for relevance to the question
+    # If a document is relevant, add it to the filtered_docs list. If not, set is_web_search to True
     for doc in docs:
         result = grader_chain.invoke({"question": question, "document": doc})
         if  result.is_doc_relevant:
             filtered_docs.append(doc)
         else:
             is_web_search = True
-    return{
+            print("==Irrelevant document detected. Setting is_web_search to True.==")
+    return {
         "documents" : filtered_docs,
         "is_web_search" : is_web_search
     }
