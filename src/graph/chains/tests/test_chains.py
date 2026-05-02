fd49a3cb064b093f03e2aed2cb036236d37cbac9
Vamsi <mkrishnadev1996@gmail.com>
2026-05-02 14:45:45 +0530

    refactored the ingestion and added tests

---
+++ src/graph/chains/tests/test_chains.py
@@ -0,0 +1,25 @@
+from src.graph.chains import grader
+from src.ingestion import retriever
+
+
+def test_grader_chain_output_format():
+    question = "What is Agent memory"
+    docs = retriever.invoke(question)
+    if docs:
+        first_doc = docs[0].page_content
+        result = grader.grader_chain.invoke(
+            {"question": question,
+            "document": first_doc}
+        )
+        assert isinstance(result.relevance_score, int) and  isinstance(result.is_doc_relevant, bool), "relevance_score should be an integer.is_doc_relevant should be a boolean. "
+
+def test_grader_chain_is_doc_relevant():
+    question = "What is Agent memory"
+    docs = retriever.invoke(question)
+    if docs:
+        first_doc = docs[0].page_content
+        result = grader.grader_chain.invoke(
+            {"question": question,
+            "document": first_doc}
+        )
+        assert result.is_doc_relevant == True, "The document should be relevant to the question"
--- src/ingestion.py
+++ src/ingestion.py
@@ -13,27 +13,26 @@ URLS =[
     "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
     "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
 ]
-
-# Get docs and split into chunks
-try:
-    docs = WebBaseLoader(URLS).load()
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, 
-        chunk_overlap=200
-    )
-    doc_splits = text_splitter.split_documents(docs)
-    print(f"Total chunks created: {len(doc_splits)}")
-except Exception as e:
-    print(f"Error loading or processing documents: {e}")
-
 embeddings = HuggingFaceEndpointEmbeddings(
         repo_id=EMBEDDING_MODEL,
         task="feature-extraction"
     )
 
-# Check if the ChromaDB directory already exists to avoid re-creating the vectorstore
+# Get docs and split into chunks
 if not os.path.exists("./chroma_db"):
+    try:
+        docs = WebBaseLoader(URLS).load()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, 
+            chunk_overlap=200
+        )
+        doc_splits = text_splitter.split_documents(docs)
+        print(f"Total chunks created: {len(doc_splits)}")
+    except Exception as e:
+        print(f"Error loading or processing documents: {e}")
+
+
     try:
         vectorstore = Chroma.from_documents(
             documents=doc_splits, 
